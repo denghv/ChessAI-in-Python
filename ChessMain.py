7179fb60218d04276edfd24e0ecbb0c84d6af14a
denghv <137135014+denghv@users.noreply.github.com>
2023-12-17 21:25:24 +0700

    Update ChessMain.py

--- ChessMain.py
+++ ChessMain.py
@@ -19,8 +19,8 @@ IMAGES = {}
 
 def loadImages():
     """
-    Initialize a global directory of images.
-    This will be called exactly once in the main.
+    Khởi tạo thư mục toàn cục của hình ảnh
+    Được gọi chính xác mỗi lần ở hàm main
     """
     pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
@@ -29,8 +29,8 @@ def loadImages():
 
 def main():
     """
-    The main driver for our code.
-    This will handle user input and updating the graphics.
+    Chương trình điều khiển mã nguồn
+    Xử lý input và cập nhật đồ họa
     """
     p.init()
     screen = p.display.set_mode((BOARD_WIDTH + MOVE_LOG_PANEL_WIDTH, BOARD_HEIGHT))
@@ -38,19 +38,19 @@ def main():
     screen.fill(p.Color("white"))
     game_state = ChessEngine.GameState()
     valid_moves = game_state.getValidMoves()
-    move_made = False  # flag variable for when a move is made
-    animate = False  # flag variable for when we should animate a move
-    loadImages()  # do this only once before while loop
+    move_made = False  # biến cờ kiểm tra khi thực hiện một nước cờ ;
+    animate = False  # biến cờ thực hiện khi nên tạo hiệu ứng cho nước cờ;
+    loadImages()  # thực hiện duy nhất 1 lần trước vòng while
     running = True
-    square_selected = ()  # no square is selected initially, this will keep track of the last click of the user (tuple(row,col))
-    player_clicks = []  # this will keep track of player clicks (two tuples)
+    square_selected = ()  # ban đầu không có ô nào được chọn, biến theo dõi clicks cuối của người chơi (tuple(row,col));
+    player_clicks = []  # biến theo dõi những lần clicks của người chơi (2 tuple [(6,4), (4,4)])
     game_over = False
     ai_thinking = False
     move_undone = False
     move_finder_process = None
     move_log_font = p.font.SysFont("Arial", 14, False, False)
-    player_one = True  # if a human is playing white, then this will be True, else False
-    player_two = False  # if a hyman is playing white, then this will be True, else False
+    player_one = True  # nếu người chơi đang chơi quân trắng, trả về True, không sẽ trả về False;
+    player_two = False  # nếu người chơi đang chơi quân đen, trả về True, không sẽ trả về False;
 
     while running:
         human_turn = (game_state.white_to_move and player_one) or (not game_state.white_to_move and player_two)
@@ -58,33 +58,33 @@ def main():
             if e.type == p.QUIT:
                 p.quit()
                 sys.exit()
-            # mouse handler
+            # bộ xử lý thao tác chuột
             elif e.type == p.MOUSEBUTTONDOWN:
                 if not game_over:
-                    location = p.mouse.get_pos()  # (x, y) location of the mouse
+                    location = p.mouse.get_pos()  # (x, y) vị trí của chuột
                     col = location[0] // SQUARE_SIZE
                     row = location[1] // SQUARE_SIZE
-                    if square_selected == (row, col) or col >= 8:  # user clicked the same square twice
-                        square_selected = ()  # deselect
-                        player_clicks = []  # clear clicks
+                    if square_selected == (row, col) or col >= 8:  # người dùng nhấp vào cùng ô 2 lần;
+                        square_selected = ()  # bỏ chọn
+                        player_clicks = []  # xóa bỏ các lần clicks
                     else:
                         square_selected = (row, col)
-                        player_clicks.append(square_selected)  # append for both 1st and 2nd click
-                    if len(player_clicks) == 2 and human_turn:  # after 2nd click
+                        player_clicks.append(square_selected)  # thêm vào cả lần nhấp đầu và lần nhấp thứ hai;
+                    if len(player_clicks) == 2 and human_turn:  # sau lần nhấp thứ 2
                         move = ChessEngine.Move(player_clicks[0], player_clicks[1], game_state.board)
                         for i in range(len(valid_moves)):
                             if move == valid_moves[i]:
                                 game_state.makeMove(valid_moves[i])
                                 move_made = True
                                 animate = True
-                                square_selected = ()  # reset user clicks
+                                square_selected = ()  # reset lần nhấp của người dùng ;
                                 player_clicks = []
                         if not move_made:
                             player_clicks = [square_selected]
 
-            # key handler
+            # bộ xử lý phím
             elif e.type == p.KEYDOWN:
-                if e.key == p.K_z:  # undo when 'z' is pressed
+                if e.key == p.K_z:  # hoàn tác khi nhấn 'z';
                     game_state.undoMove()
                     move_made = True
                     animate = False
@@ -93,7 +93,7 @@ def main():
                         move_finder_process.terminate()
                         ai_thinking = False
                     move_undone = True
-                if e.key == p.K_r:  # reset the game when 'r' is pressed
+                if e.key == p.K_r:  # reset trận đấu khi nhấn 'r';
                     game_state = ChessEngine.GameState()
                     valid_moves = game_state.getValidMoves()
                     square_selected = ()
@@ -106,11 +106,11 @@ def main():
                         ai_thinking = False
                     move_undone = True
 
-        # AI move finder
+        # tìm kiếm nước đi của AI
         if not game_over and not human_turn and not move_undone:
             if not ai_thinking:
                 ai_thinking = True
-                return_queue = Queue()  # used to pass data between threads
+                return_queue = Queue()  # sử dụng để truyền dữ liệu giữa các luồng;
                 move_finder_process = Process(target=ChessAI.findBestMove, args=(game_state, valid_moves, return_queue))
                 move_finder_process.start()
 
@@ -153,7 +153,7 @@ def main():
 
 def drawGameState(screen, game_state, valid_moves, square_selected):
     """
-    Responsible for all the graphics within current game state.
+    Chịu trách nhiệm về tất cả đồ họa trong trạng thái trò chơi hiện tại.
     """
     drawBoard(screen)  # draw squares on the board
     highlightSquares(screen, game_state, valid_moves, square_selected)
@@ -162,8 +162,8 @@ def drawGameState(screen, game_state, valid_moves, square_selected):
 
 def drawBoard(screen):
     """
-    Draw the squares on the board.
-    The top left square is always light.
+    Vẽ các ô trên bàn cờ.
+    Ô ở góc trên bên trái luôn có màu sáng.
     """
     global colors
     colors = [p.Color("white"), p.Color("gray")]
@@ -175,7 +175,7 @@ def drawBoard(screen):
 
 def highlightSquares(screen, game_state, valid_moves, square_selected):
     """
-    Highlight square selected and moves for piece selected.
+    Đánh dấu ô đã chọn và các nước đi cho quân cờ đã chọn.
     """
     if (len(game_state.move_log)) > 0:
         last_move = game_state.move_log[-1]
@@ -187,12 +187,12 @@ def highlightSquares(screen, game_state, valid_moves, square_selected):
         row, col = square_selected
         if game_state.board[row][col][0] == (
                 'w' if game_state.white_to_move else 'b'):  # square_selected is a piece that can be moved
-            # highlight selected square
+            # đánh dấu ô đã chọn
             s = p.Surface((SQUARE_SIZE, SQUARE_SIZE))
-            s.set_alpha(100)  # transparency value 0 -> transparent, 255 -> opaque
+            s.set_alpha(100)  # giá trị trong suốt 0 -> trong suốt, 255 -> không trong suốt
             s.fill(p.Color('blue'))
             screen.blit(s, (col * SQUARE_SIZE, row * SQUARE_SIZE))
-            # highlight moves from that square
+            # đánh dấu nước đi từ ô đó
             s.fill(p.Color('yellow'))
             for move in valid_moves:
                 if move.start_row == row and move.start_col == col:
@@ -201,7 +201,7 @@ def highlightSquares(screen, game_state, valid_moves, square_selected):
 
 def drawPieces(screen, board):
     """
-    Draw the pieces on the board using the current game_state.board
+    Vẽ các quân cờ trên bàn cờ sử dụng game_state.board hiện tại
     """
     for row in range(DIMENSION):
         for column in range(DIMENSION):
@@ -212,8 +212,7 @@ def drawPieces(screen, board):
 
 def drawMoveLog(screen, game_state, font):
     """
-    Draws the move log.
-
+    Vẽ nhật ký nước đi.
     """
     move_log_rect = p.Rect(BOARD_WIDTH, 0, MOVE_LOG_PANEL_WIDTH, MOVE_LOG_PANEL_HEIGHT)
     p.draw.rect(screen, p.Color('black'), move_log_rect)
@@ -253,32 +252,32 @@ def drawEndGameText(screen, text):
 
 def animateMove(move, screen, board, clock):
     """
-    Animating a move
+    Đang diễn nước đi
     """
     global colors
     d_row = move.end_row - move.start_row
     d_col = move.end_col - move.start_col
-    frames_per_square = 10  # frames to move one square
+    frames_per_square = 10  # khung để di chuyển một ô
     frame_count = (abs(d_row) + abs(d_col)) * frames_per_square
     for frame in range(frame_count + 1):
         row, col = (move.start_row + d_row * frame / frame_count, move.start_col + d_col * frame / frame_count)
         drawBoard(screen)
         drawPieces(screen, board)
-        # erase the piece moved from its ending square
+        # xóa quân cờ đã di chuyển khỏi ô đích
         color = colors[(move.end_row + move.end_col) % 2]
         end_square = p.Rect(move.end_col * SQUARE_SIZE, move.end_row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
         p.draw.rect(screen, color, end_square)
-        # draw captured piece onto rectangle
+        # vẽ quân cờ bị bắt lên hình chữ nhật
         if move.piece_captured != '--':
             if move.is_enpassant_move:
                 enpassant_row = move.end_row + 1 if move.piece_captured[0] == 'b' else move.end_row - 1
                 end_square = p.Rect(move.end_col * SQUARE_SIZE, enpassant_row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
             screen.blit(IMAGES[move.piece_captured], end_square)
-        # draw moving piece
+        # vẽ quân cờ đang di chuyển
         screen.blit(IMAGES[move.piece_moved], p.Rect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
         p.display.flip()
         clock.tick(60)
 
 
 if __name__ == "__main__":
-    main()g
+    main()
